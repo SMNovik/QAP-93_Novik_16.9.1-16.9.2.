6c864a4f50daf07023f4b46fa935af41d833524f
Sergey Novik <110816135+SMNovik@users.noreply.github.com>
2022-09-30 19:09:52 +0300

    Add files via upload

---
+++ ex.16.9.1-16.9.2(метод __str__).py
@@ -0,0 +1,20 @@
+﻿# 16.9.1-16.9.2
+
+class Rectangle:
+    def __init__(self,x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f"Координаты {self.x} и {self.y}. Ширина {self.width}, высота {self.height}"
+
+    def Area(self):
+        return self.width * self.height
+
+r = Rectangle(5, 10, 50, 100)
+print(r)
+
+r = Rectangle(5,10, 50, 100)
+print(f"Площадь прямоугольника - {r.Area()}")
